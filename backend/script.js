e45f277195161a0677a0cbcbe957c4863c2cc9ca
HimanshuBisht23L <himanshiubisht4532@gmail.com>
2025-04-25 12:47:56 +0530

    buttons added

--- backend/script.js
+++ backend/script.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+// import fs from 'fs'
 
 const app = express();
 app.use(cors());
@@ -7,7 +8,16 @@ app.use(express.json());
 
 app.post('/recieve', (req, res) => {
   console.log('Received node sequence:', req.body);
-  res.json({ message: 'Nodes received successfully!' });
+  // fs.writeFile('output.json', req.body, (err) => {
+  //   if (err) {
+  //     console.error('Error writing to file:', err);
+  //   } else {
+  //     console.log('Array saved to myArray.json');
+  //   }
+  // };
+
+
+    res.json({ message: 'Nodes received successfully!' })
 });
 
 app.listen(3000, () => {
